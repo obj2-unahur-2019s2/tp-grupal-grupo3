0e2a4cac7e05f3cc642c36f1066f73ec3e73e504
fragata-adrian <fragataadrian99@gmail.com>
2019-10-29 20:33:59 -0300

    master iniciado

---
+++ src/test/java/mainTest.java
@@ -0,0 +1,12 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class mainTest {
+
+    @Test
+    public void testMain() {
+        Assert.assertEquals(true,true);
+    }
+}
